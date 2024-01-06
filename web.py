871e5c8094c1694e3457111e6fc81ee9f9aa8ed2
Sravani2108 <140194996+Sravani2108@users.noreply.github.com>
2024-01-06 12:55:34 +0530

    Add files via upload

---
+++ web.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pickle
+
+model=pickle.load(open('model1.pkl', 'rb'))
+
+st.title('Salary prediction system')
+
+yoe=st.text_input("Enter years of experience: ")
+
+if st.button("predict"):
+    yoe=float (yoe)
+    data=[[yoe]]
+    result=model.predict(data)
+    st.success(result)
